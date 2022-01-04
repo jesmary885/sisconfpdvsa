4b76255fb9092dcdcc0c635b243170d5f6bd86e8
jesmary <jesmary885@gmail.com>
2022-01-04 07:49:01 -0400

    cambios 4-1-22

--- resources/views/livewire/gestion-asignacion.blade.php
+++ resources/views/livewire/gestion-asignacion.blade.php
@@ -5,8 +5,8 @@
         <div class="items-center pt-4">
             <p class="text-gray-700 mt-6">Objetivo Estrategico: </p>
         </div>
-        <div class="col-span-3 pt-5">
-            <select wire:model="objestrategicos_id" class="w-full form-control mt-4 h-8 text-xs">
+        <div class="col-span-3 pt-5 px-4">
+            <select wire:model="objestrategicos_id" class="w-full form-control mt-4 h-8 text-xs outline-none">
                 <option value="" selected>Seleccione un Objetivo Estrategico</option>
                 @foreach ($objestrategicos as $objestrategico)
                     <option value="{{$objestrategico->id}}">{{$objestrategico->description}}</option>
@@ -20,8 +20,8 @@
         <div class="items-center pr-4">
             <p class="text-gray-700 mt-6">Objetivo Tactico: </p>
         </div>
-        <div class="items-center col-span-3 pt-2">
-            <select wire:model="objtacticos_id" class="w-full form-control mt-4 h-8 text-xs">
+        <div class="items-center col-span-3 pt-2 px-4">
+            <select wire:model="objtacticos_id" class="w-full form-control mt-4 h-8 text-xs outline-none">
                 <option value="" selected disabled>Seleccione un Objetivo Tactico</option>
                 @foreach ($tacticos as $tactico)
                     <option value="{{$tactico->id}}">{{$tactico->description}}</option>
@@ -35,8 +35,8 @@
         <div class="tems-center pt-4">
             <p class="text-gray-700 pr-8">Objetivo Operacional: </p>
         </div>
-        <div class="col-span-3 pt-2">
-            <select wire:model="objoperacionals_id" class="w-full form-control mt-4 h-8 text-xs">
+        <div class="col-span-3 pt-2 px-4">
+            <select wire:model="objoperacionals_id" class="w-full form-control mt-4 h-8 text-xs outline-none">
                 <option value="" selected disabled>Seleccione un Objetivo Operacional</option>
                 @foreach ($operacionals as $operacional)
                     <option value="{{$operacional->id}}">{{$operacional->description}}</option>
@@ -51,13 +51,13 @@
             <p class="text-gray-700 pr-8">Conformación de ENT:</p>
         </div>
         <div class="col-span-3 pt-6 w-full">
-            <div class="flex justify-items-stretch w-full mr-6"> 
+            <div class="flex justify-items-stretch w-full mr-6 px-4"> 
                 <div>
-                    <x-input.date wire:model.lazy="input_conformacion_i" id="input_conformacion_i" placeholder="Seleccione la fecha" class="px-6" />
+                    <x-input.date wire:model.lazy="input_conformacion_i" id="input_conformacion_i" placeholder="Seleccione la fecha" class="px-4 outline-none" />
                     <x-input-error for="input_conformacion_i" />
                 </div>
                 <div class="ml-12">
-                    <x-input.date wire:model.lazy="input_conformacion_f" id="input_conformacion_f" placeholder="Seleccione la fecha" class="px-6" />
+                    <x-input.date wire:model.lazy="input_conformacion_f" id="input_conformacion_f" placeholder="Seleccione la fecha" class="px-4 outline-none" />
                     <x-input-error for="input_conformacion_f" />
                 </div>
             </div> 
@@ -69,13 +69,13 @@
             <p class="text-gray-700 pr-8">Recopilación de información, Desarrollo y Analisis:</p>
         </div>
         <div class="col-span-3 pt-6 w-full">
-            <div class="flex justify-items-stretch w-full mr-6">
+            <div class="flex justify-items-stretch w-full mr-6 px-4">
                 <div>
-                    <x-input.date wire:model.lazy="input_recopilacion_i" id="input_recopilacion_i" placeholder="Seleccione la fecha" class="px-6"/>
+                    <x-input.date wire:model.lazy="input_recopilacion_i" id="input_recopilacion_i" placeholder="Seleccione la fecha" class="px-4 outline-none"/>
                     <x-input-error for="input_recopilacion_i" />
                 </div>
                 <div class="ml-12">
-                    <x-input.date wire:model.lazy="input_recopilacion_f" id="input_recopilacion_f" placeholder="Seleccione la fecha" class="px-6"/>
+                    <x-input.date wire:model.lazy="input_recopilacion_f" id="input_recopilacion_f" placeholder="Seleccione la fecha" class="px-4 outline-none"/>
                     <x-input-error for="input_recopilacion_f" />
                 </div>
             </div>
@@ -87,13 +87,13 @@
             <p class="text-gray-700 pr-8">Información Preliminar:</p>
         </div>
         <div class="col-span-3 pt-6 w-full">
-            <div class="flex justify-items-stretch w-full mr-6">
+            <div class="flex justify-items-stretch w-full mr-6 px-4">
                 <div>
-                    <x-input.date wire:model.lazy="input_inf_i" id="input_inf_i" placeholder="Seleccione la fecha" class="px-6"/>
+                    <x-input.date wire:model.lazy="input_inf_i" id="input_inf_i" placeholder="Seleccione la fecha" class="px-4 outline-none"/>
                     <x-input-error for="input_inf_i" />
                 </div>
                 <div class="ml-12">
-                    <x-input.date wire:model.lazy="input_inf_f" id="input_inf_f" placeholder="Seleccione la fecha" class="px-6"/>
+                    <x-input.date wire:model.lazy="input_inf_f" id="input_inf_f" placeholder="Seleccione la fecha" class="px-4 outline-none"/>
                     <x-input-error for="input_inf_f" />
                 </div>
             </div>
@@ -105,13 +105,13 @@
             <p class="text-gray-700 pr-8">Divulgación y Publicación:</p>
         </div>
         <div class="col-span-3 pt-6 w-full">
-            <div class="flex justify-items-stretch w-full mr-6">
+            <div class="flex justify-items-stretch w-full mr-6 px-4">
                 <div>
-                    <x-input.date wire:model.lazy="input_divulgacion_i" id="input_divulgacion_i" placeholder="Seleccione la fecha" class="px-6"/>
+                    <x-input.date wire:model.lazy="input_divulgacion_i" id="input_divulgacion_i" placeholder="Seleccione la fecha" class="px-4 outline-none"/>
                     <x-input-error for="input_divulgacion_i" />
                 </div>
                 <div class="ml-12">
-                    <x-input.date wire:model.lazy="input_divulgacion_f" id="input_divulgacion_f" placeholder="Seleccione la fecha" class="px-6"/>
+                    <x-input.date wire:model.lazy="input_divulgacion_f" id="input_divulgacion_f" placeholder="Seleccione la fecha" class="px-4 outline-none"/>
                     <x-input-error for="input_divulgacion_f" />
                 </div>
             </div>
@@ -123,29 +123,15 @@
             <p class="text-gray-700 pr-8">Carga de Recomendaciones:</p>
         </div>
         <div class="col-span-3 pt-6 w-full">
-            <div class="flex justify-items-stretch w-full mr-6 pr-4">
+            <div class="flex justify-items-stretch w-full mr-6 px-4">
                 <div>
-                    <x-input.date wire:model.lazy="input_carga_i" id="input_carga_i" placeholder="Seleccione la fecha" class="px-6"/>
+                    <x-input.date wire:model.lazy="input_carga_i" id="input_carga_i" placeholder="Seleccione la fecha" class="px-4 outline-none"/>
                     <x-input-error for="input_carga_i" />
                 </div>
                 <div class="ml-12">
-                    <x-input.date wire:model.lazy="input_carga_f" id="input_carga_f" placeholder="Seleccione la fecha" class="px-6"/>
+                    <x-input.date wire:model.lazy="input_carga_f" id="input_carga_f" placeholder="Seleccione la fecha" class="px-4 outline-none"/>
                     <x-input-error for="input_carga_f" />
                 </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="grid grid-cols-4">
-        <div class="tems-center pt-6">
-            <p class="text-gray-700 pr-8">Instalación:</p>
-        </div>
-        <div class="col-span-3 pt-6 w-full">
-            <div class="flex justify-items-stretch w-full pr-2">
-                <div class="w-full">
-                    <x-input wire:model="instalacion" class="block mt-1 outline-none w-full px-2" type="text" name="instalacion" id="instalacion" required />
-                </div>
-                
             </div>
         </div>
     </div>
@@ -154,8 +140,8 @@
         <div class="items-center pt-4">
             <p class="text-gray-700 mt-6">Región del Usuario: </p>
         </div>
-        <div class="col-span-3 pt-5">
-            <select wire:model="region_id" class="w-full form-control mt-4 h-8 text-xs" >
+        <div class="col-span-3 pt-5 px-4">
+            <select wire:model="region_id" class="w-full form-control mt-4 h-8 text-xs outline-none" >
                 <option value="" selected>Seleccionar Región</option>
                 @foreach ($regions as $region)
                     <option value="{{$region->id}}">{{$region->name}}</option>
@@ -169,8 +155,8 @@
         <div class="items-center pt-4">
             <p class="text-gray-700 mt-6">División del Usuario: </p>
         </div>
-        <div class="col-span-3 pt-5">
-            <select wire:model="division_id" class="w-full form-control mt-4 h-8 text-xs">
+        <div class="col-span-3 pt-5 px-4">
+            <select wire:model="division_id" class="w-full form-control mt-4 h-8 text-xs outline-none">
                 <option value="" selected>Seleccionar División</option>
                 @foreach ($divisions as $division)
                     <option value="{{$division->id}}">{{$division->name}}</option>
@@ -184,8 +170,8 @@
         <div class="items-center pt-4">
             <p class="text-gray-700 mt-6">Negocio del Usuario: </p>
         </div>
-        <div class="col-span-3 pt-5">
-            <select wire:model="negocio_id" class="w-full form-control mt-4 h-8 text-xs">
+        <div class="col-span-3 pt-5 px-4">
+            <select wire:model="negocio_id" class="w-full form-control mt-4 h-8 text-xs outline-none">
                 <option value="" selected>Seleccionar Negocio</option>
                 @foreach ($negocios as $negocio)
                     <option value="{{$negocio->id}}">{{$negocio->name}}</option>
@@ -199,14 +185,28 @@
         <div class="items-center pt-4">
             <p class="text-gray-700 mt-6">Usuario: </p>
         </div>
-        <div class="col-span-3 pt-5">
-            <select wire:model="usuario_id" class="w-full form-control mt-4 h-8 text-xs">
+        <div class="col-span-3 pt-5 px-4">
+            <select wire:model="usuario_id" class="w-full form-control mt-4 h-8 text-xs outline-none">
                 <option value="" selected>Seleccionar Usuario</option>
                 @foreach ($usuarios as $usuario)
                     <option value="{{$usuario->id}}">{{$usuario->name }} {{$usuario->apellido}}</option>
                 @endforeach
             </select>
             <x-input-error for="usuario_id" />
+        </div>
+    </div>
+
+    <div class="grid grid-cols-4">
+        <div class="tems-center pt-6">
+            <p class="text-gray-700 pr-8">Instalación:</p>
+        </div>
+        <div class="col-span-3 pt-6 w-full">
+            <div class="flex justify-items-stretch w-full px-4">
+                <div class="w-full">
+                    <x-input wire:model="instalacion" class="block mt-1 outline-none w-full px-2" type="text" name="instalacion" id="instalacion" required />
+                </div>
+                
+            </div>
         </div>
     </div>
 
